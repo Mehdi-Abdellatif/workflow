d6af39dd460943dcce6bb2d627669db7a717a95b
Mehdi Abdellatif <mehdiabdellatif04@gmail.com>
2025-02-18 14:45:09 +0100

    Update app.config.ts
    Added imports/providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
+import { registerLicense } from '@syncfusion/ej2-base';
+
 import { routes } from './app.routes';
 
+registerLicense('ORg4AjUWIQA/Gnt2XVhhQlJHfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTH5Rd0JjUXxbdHNcQWVa');
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(DiagramModule)
+  ],
 };
+
+
